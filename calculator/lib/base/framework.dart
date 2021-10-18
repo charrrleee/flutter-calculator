b564b5b2cb5c85c49be7fbe30dfd10246b418f0c
charrrleee <charlcpy@gmail.com>
2021-10-18 09:32:33 +0800

    init

---
+++ calculator/lib/base/framework.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// todo move to custom library
+abstract class BaseStatelessWidget extends StatelessWidget {}
+
+abstract class BaseState<T extends StatefulWidget> extends State<T> {}
+
+abstract class BaseMVVMState<T extends StatefulWidget, VM extends BaseViewModel>
+    extends BaseState<T> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (_) => buildViewModel(),
+        child: Consumer<VM>(
+          builder: (ctx, vm, _) {
+            return buildChild(ctx, vm);
+          },
+        ));
+  }
+
+  VM buildViewModel();
+
+  Widget buildChild(ctx, VM vm);
+}
+
+abstract class BaseViewModel extends ChangeNotifier {}
